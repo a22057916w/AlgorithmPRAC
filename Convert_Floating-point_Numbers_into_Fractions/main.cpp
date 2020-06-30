ec204c4675fdeffd72f5a55728ccd0a216cb9eba
naitea9527 <willy@willylaptop.localdomain>
2020-06-30 18:38:03 +0800

    convert double-reading to string-reading

--- Convert_Floating-point_Numbers_into_Fractions/main.cpp
+++ Convert_Floating-point_Numbers_into_Fractions/main.cpp
@@ -26,16 +26,25 @@ unsigned_int64 GCD(unsigned_int64 a, unsigned_int64 b) {
 
 int main(int argc, char *argv[]) {
   string snum = argv[1];
-  double fnum = stod(snum);
+  //double fnum = stod(snum);
   //string dot = string(".");
-  cout << fnum << endl;
-  unsigned_int64 digit = 0;
-  double tmp = fnum;
+  //cout << fnum << endl;
+  if(snum.find('.') != string::npos)
+    snum.erase(find(snum.begin(), snum.end(), '.'));
+  unsigned_int64 digit = snum.length();
+  unsigned_int64 N = stod(snum);
+  unsigned_int64 D = pow(10, digit - 1);
+  unsigned_int64 g = GCD(D, N);
+
+  // make GCD(N, D) to be relative prime, print the answer
+  cout << N / g << "/" << D / g << endl;
+
+  //double tmp = fnum;
 
   // calculate total digits
   // break when last digit is 0. e.g. 3140
   // total digits would be one more than original one.
-  while(ceil(tmp / 10) != int(tmp / 10) ) {
+  /*while(ceil(tmp / 10) != int(tmp / 10) ) {
     tmp *= 10;
     digit++;
   }
@@ -49,7 +58,7 @@ int main(int argc, char *argv[]) {
   unsigned_int64 g = GCD(D, N);
 
   // make GCD(N, D) to be relative prime, print the answer
-  cout << N / g << "/" << D / g << endl;
+  cout << N / g << "/" << D / g << endl;*/
 
   return 0;
 }
