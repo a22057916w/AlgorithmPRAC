31bd694c4bfaa6f7103b6ac85c054e0675c85d8b
naitea9527 <willy@willylaptop.localdomain>
2020-06-30 17:39:46 +0800

    write gcd func

--- Convert_Floating-point_Numbers_into_Fractions/main.cpp
+++ Convert_Floating-point_Numbers_into_Fractions/main.cpp
@@ -11,20 +11,36 @@ using namespace std;
 
 typedef unsigned long long unsigned_int64;
 
-int main(int argc, char *argv[]) {
-  string snum = argv[1];
-  double fnum = stod(snum);
-  string dot = string(".");
+// self-Implement GCD, a must greater than b
+unsigned_int64 GCD(unsigned_int64 a, unsigned_int64 b) {
+  unsigned_int64 t = 1;
 
-  while(cin >> fnum && fnum != 0) {
-
-      unsigned_int64 digit;
-      double tmp = fnum;
-      while(ceil(tmp / 10) == int(tmp / 10) ) {
-        tmp *= 10;
-        digit++;
-      }
-      cout << digit << endl;
+  while(t != 0) {
+    t = a % b;
+    a = b;
+    b = t;
   }
+
+  return a;
+}
+
+int main(int argc, char *argv[]) {
+  //string snum = argv[1];
+  double fnum = 3.14;
+  //string dot = string(".");
+
+  unsigned_int64 digit = 0;
+  double tmp = fnum;
+
+  while(ceil(tmp / 10) != int(tmp / 10) ) {
+    cout << tmp << endl;
+    tmp *= 10;
+    digit++;
+  }
+
+  unsigned_int64 N = digit;
+  unsigned_int64 D = digit + 1;
+  cout << digit << endl;
+
   return 0;
 }
