a70396cf919ccc510bcbad5e88367f63ed7396ee
naitea9527 <willy@willylaptop.localdomain>
2020-07-03 14:29:15 +0800

    test phase 1

--- Square_Matrix_Traversal/main.cpp
+++ Square_Matrix_Traversal/main.cpp
@@ -18,12 +18,22 @@ int main(int argc, char *argv[]) {
   for(LLU i = 0; i < m; i++)
     ans += 2 * i;
 
-  if(c % 1)
-    for(LLU i = 0; i < m - r; i++)
-      ans++;
-  else
-    for(LLU i = 0; i < m - r; i++)
-      ans--;
+  if(m == c) {
+    if(c % 1)
+      for(LLU i = 0; i < m - r; i++)
+        ans++;
+    else
+      for(LLU i = 0; i < m - r; i++)
+        ans--;
+  }
+  else {
+    if(r % 1)
+      for(LLU i = 0; i < m - c; i++)
+        ans--;
+    else
+      for(LLU i = 0; i < m - c; i++)
+        ans++;
+  }
 
   cout << ans << endl;
 
