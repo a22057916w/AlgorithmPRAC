e6b4b69c293154d69941f1b71a07dbf4593d80cb
naitea9527 <willy@willylaptop.localdomain>
2020-07-03 14:04:29 +0800

    implement new method

--- Square_Matrix_Traversal/main.cpp
+++ Square_Matrix_Traversal/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
+#include <alogrithm>
 using namespace std;
 
 typedef unsigned long long LLU;
@@ -9,10 +9,23 @@ typedef unsigned long long LLU;
 
 int main(int argc, char *argv[]) {
 
-  LLU i, j;
-  cin >> i >> j;
+  LLU r, c;
+  cin >> r >> c;
 
-  cout << order(i, j) << endl;
+  LLU m = max(r, c);
+
+  LLU ans = 1;
+  for(LLU i = 0; i < m; i++)
+    ans += 2 * i;
+
+  if(c % 1)
+    for(LLU i = 0; i < r; i++)
+      ans++;
+  else
+    for(LLU i = 0; i < r; i++)
+      ans--;
+
+  cout << ans << endl;
 
   return 0;
 }
