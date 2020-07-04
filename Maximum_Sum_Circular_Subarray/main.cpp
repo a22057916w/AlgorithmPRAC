a9a077e533d290f6d5fc89ca016096abe6b466dd
naitea9527 <willy@willylaptop.localdomain>
2020-07-04 19:17:06 +0800

    phase 1 fix

--- Maximum_Sum_Circular_Subarray/main.cpp
+++ Maximum_Sum_Circular_Subarray/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
-#include <alogrithm>
+#include <algorithm>
 using namespace std;
 
 typedef unsigned long long LLU;
 
-int MAXN = 100005;
+const int MAXN = 100005;
+int a[MAXN];
 
 LLU greedy(int len) {
-  LLU dp[MAXN] = {0};
-  int s, e;
+  LLU sum = 0, max_sum = 0;
+  int start = 0, end = 0, temp_start = 0;
 
   for(int i = 0; i < len; i++) {
-    dp[i] = max(dp[i -1] + a[i], a[i]);
+    sum += a[i];
+
+    if(sum < 0) {
+      sum = 0;
+      temp_start = i + 1;
+    }
+
+    if(sum > max_sum) {
+      max_sum = sum;
+      start = temp_start;
+      end = i;
+    }
   }
 
+  return max_sum;
 }
 
 int main(int argc, char *argv[]) {
